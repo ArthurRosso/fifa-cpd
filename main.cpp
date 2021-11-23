c51ac6922059a2b108eaf8b6fe0a3b4c25045c8e
ArthurRosso <arthurderosso@gmail.com>
2021-11-23 02:41:06 -0300

    add hash table

--- main.cpp
+++ main.cpp
@@ -5,15 +5,27 @@
 #include <vector>
 #include <list>
 #include <algorithm>
+#include <cstring>
 #include "./csv-parser/parser.hpp"
 #include "player.h"
 #include "user.h"
-#include "triePlayer.h"
+#include "trie_player.h"
 #include "player_pointer.h"
-#include "tags.h"
+#include "ht.h"
+// #include "tags.h"
 
 using namespace aria::csv;
 using namespace std;
+
+#define MAX_POS 15
+
+/* sorts the linked list by changing next pointers (not data) */
+void sortPlayerPointer(PlayerPointer** headRef, Player* root);
+
+void FrontBackSplit(PlayerPointer* source, PlayerPointer** frontRef, PlayerPointer** backRef);
+
+PlayerPointer* SortedMerge(PlayerPointer* a, PlayerPointer* b, Player* root);
+
 
 int main (){
 	Player *player_root = NULL;
@@ -22,10 +34,11 @@ int main (){
 	User *user_current = NULL;
     TriePlayer *trie_root = newTriePlayer();
 	list<int>* ids = new list<int>;
-    PlayerPointer* positions[15]={NULL};
+    PlayerPointer* positions[MAX_POS]={NULL};
 	string pos, line, cmd, item;
 	bool ishead=true;
-	
+	time_t start, ending;
+	time(&start);
 
 	ifstream p("./data/players.csv");
     CsvParser players(p);
@@ -66,6 +79,49 @@ int main (){
     }
 	r.close();
 
+	ifstream t("./data/tags.csv");
+    CsvParser tags(t);
+
+	ishead=true;
+
+	ht* hash = ht_create();
+
+    for (auto& row : tags) {
+		if(ishead){
+			ishead=false;
+			continue;
+		}
+		ht_set(hash, (row[2]).c_str(), stoi(row[1]));
+    }
+	t.close();
+
+	// for(int i=0; i<MAX_POS; i++){
+	// 	PlayerPointer* temp = positions[i];
+	// 	PlayerPointer* prev = NULL;
+
+	// 	while(temp != NULL){
+	// 		player_current = findPlayerById(player_root, temp->id_player);
+	// 		if(player_current->count < 1000){
+	// 			if(prev != NULL){
+	// 				prev->next=temp->next;
+	// 			} else {
+	// 				temp=temp->next;
+	// 			}
+	// 		}
+	// 		prev = temp;
+	// 		temp = temp->next;
+	// 	}
+	// }
+
+	// for(int i=0; i<MAX_POS; i++){
+	// 	sortPlayerPointer(&positions[i], player_root);
+	// }
+
+	time(&ending);
+
+	cout << "Total: " << difftime(ending, start) << " seconds to initialize." << endl;
+	
+
 	while(true){
 
         cout << "\n$> ";
@@ -80,10 +136,28 @@ int main (){
 
 			findPlayerByName(trie_root, item, ids);
 			if(!ids->empty()){
-				cout << "sofifa_id" << " " << " name "<< "  " <<" player_positions "<< " " <<" rating "<< " " <<" count" << endl;
+				cout.width(10);
+				cout << "sofifa_id";
+				cout.width(50);
+				cout << "name";
+				cout.width(20);
+				cout << "player_positions";
+				cout.width(10);
+				cout << "rating";
+				cout.width(8);
+				cout << "count" << endl;
 				for (list<int>::iterator it=ids->begin(); it!=ids->end(); it++){
 					player_current = findPlayerById(player_root, *it);
-					cout << player_current->id << "  " << player_current->name << "  " << player_current->positions << " " << player_current->rating << " " << player_current->count << endl; // print player
+					cout.width(10);
+					cout << player_current->id;
+					cout.width(50);
+					cout << player_current->name;
+					cout.width(20);
+					cout << player_current->positions;
+					cout.width(10);
+					cout << (float)(player_current->rating)/(player_current->count);
+					cout.width(8);
+					cout << player_current->count << endl;
 				}
 			} else {
 				cout << "Nome ou prefixo não encontrado :(" << endl;
@@ -91,39 +165,107 @@ int main (){
 
 		} else if(cmd.compare("user")==0){
 			ss >> item;
-			ids->clear();
-
 			user_current = findUserById(user_root, stoi(item));
 			if(user_current != NULL){
-				sortUser(user_current->id_player, user_current->rating, 0, user_current->rating->size());
-				cout << "sofifa_id" << " " << " name "<< "  " <<" global_rating "<< " " <<" count "<< " " <<" rating" << endl;
+				sortUser(user_current->id_player, user_current->rating, user_current->rating->size());
+				cout.width(10);
+				cout << "sofifa_id";
+				cout.width(50);
+				cout << "name";
+				cout.width(20);
+				cout << "global_rating";
+				cout.width(10);
+				cout << "count";
+				cout.width(8);
+				cout << "rating" << endl;
 				for (int i=0; i < min(20, (int)user_current->id_player->size()); i++){
-					cout << user_current->id_player->at(i) << endl;
 					player_current = findPlayerById(player_root, user_current->id_player->at(i));
 					if(player_current != NULL){
-						cout << player_current->id << "  " << player_current->name << "  " << player_current->rating << " " << player_current->count << " " << user_current->rating->at(i) << endl; // print player
+						cout.width(10);
+						cout << player_current->id;
+						cout.width(50);
+						cout << player_current->name;
+						cout.width(20);
+						cout << (float)(player_current->rating)/(player_current->count);
+						cout.width(10);
+						cout << player_current->count;
+						cout.width(8);
+						cout << user_current->rating->at(i) << endl;					
 					}
 				}
 			} else {
 				cout << "Usuário não encontrado :(" << endl;
 			}
+
 		} else if(cmd[0]=='t' && cmd[1]=='o' && cmd[2]=='p'){
 			ss >> item;
 			cmd.erase(0,3);
-			// cout << stoi(cmd) << " " << item << endl;
 			int j = position2Int(item);
 			int i=0;
-			cout << "sofifa_id" << " " << " name "<< "  " <<" global_rating "<< " " <<" count "<< " " <<" rating" << endl;
-			while(positions[j]->next != NULL && i<stoi(cmd)){
-				player_current = findPlayerById(player_root, positions[j]->id_player);
-				if(player_current != NULL && player_current->count > 1){
-					cout << player_current->id << "  " << player_current->name << "  " << player_current->rating << " " << player_current->count << " " << user_current->rating->at(i) << endl; // print player
+			PlayerPointer* iteratorp=positions[j];
+			if(iteratorp != NULL){
+				cout.width(10);
+				cout << "sofifa_id";
+				cout.width(50);
+				cout << "name";
+				cout.width(20);
+				cout << "player_positions";
+				cout.width(10);
+				cout << "rating";
+				cout.width(8);
+				cout << "count" << endl;
+				while(iteratorp->next != NULL && i<stoi(cmd)){
+					player_current = findPlayerById(player_root, iteratorp->id_player);
+					if(player_current != NULL){
+						cout.width(10);
+						cout << player_current->id;
+						cout.width(50);
+						cout << player_current->name;
+						cout.width(20);
+						cout << player_current->positions;
+						cout.width(10);
+						cout << (float)(player_current->rating)/(player_current->count);
+						cout.width(8);
+						cout << player_current->count << endl;
+					}
+					iteratorp=iteratorp->next;
+					i++;
 				}
-				i++;
+			} else {
+				cout << "Posição " << item << " não encontrada ou nenhum jogador cadastrado nela :(" << endl;
 			}
 
 		} else if(cmd.compare("tags")==0){
-			cout << "Nenhuma tag encontrada :(" << endl;
+			ss >> item;
+			int k=0;
+			struct LinkedList* iteratort = ht_get(hash, item.c_str());
+			cout.width(10);
+			cout << "sofifa_id";
+			cout.width(50);
+			cout << "name";
+			cout.width(20);
+			cout << "player_positions";
+			cout.width(10);
+			cout << "rating";
+			cout.width(8);
+			cout << "count" << endl;
+			while(iteratort->next != NULL && k<20){
+				player_current = findPlayerById(player_root, iteratort->id);
+				if(player_current != NULL){
+					cout.width(10);
+					cout << player_current->id;
+					cout.width(50);
+					cout << player_current->name;
+					cout.width(20);
+					cout << player_current->positions;
+					cout.width(10);
+					cout << (float)(player_current->rating)/(player_current->count);
+					cout.width(8);
+					cout << player_current->count << endl;
+				}
+				iteratort=iteratort->next;
+				k++;
+			}
 			
 		} else {
 			cout << "Query escrita errada ou não implementada ainda :(" << endl;
@@ -136,4 +278,79 @@ int main (){
 
 	return 0;
 
+}
+
+
+/* sorts the linked list by changing next pointers (not data) */
+void sortPlayerPointer(PlayerPointer** headRef, Player* root){
+    PlayerPointer* head = *headRef;
+    PlayerPointer* a;
+    PlayerPointer* b;
+ 
+    /* Base case -- length 0 or 1 */
+    if ((head == NULL) || (head->next == NULL)) {
+        return;
+    }
+ 
+    /* Split head into 'a' and 'b' sublists */
+    FrontBackSplit(head, &a, &b);
+ 
+    /* Recursively sort the sublists */
+    sortPlayerPointer(&a, root);
+    sortPlayerPointer(&b, root);
+ 
+    /* answer = merge the two sorted lists together */
+    *headRef = SortedMerge(a, b, root);
+}
+
+/* UTILITY FUNCTIONS */
+/* Split the PlayerPointers of the given list into front and back halves,
+    and return the two lists using the reference parameters.
+    If the length is odd, the extra PlayerPointer should go in the front list.
+    Uses the fast/slow pointer strategy. */
+void FrontBackSplit(PlayerPointer* source, PlayerPointer** frontRef, PlayerPointer** backRef){
+    PlayerPointer* fast;
+    PlayerPointer* slow;
+    slow = source;
+    fast = source->next;
+ 
+    /* Advance 'fast' two PlayerPointers, and advance 'slow' one PlayerPointer */
+    while (fast != NULL) {
+        fast = fast->next;
+        if (fast != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+ 
+    /* 'slow' is before the midpoint in the list, so split it in two
+    at that point. */
+    *frontRef = source;
+    *backRef = slow->next;
+    slow->next = NULL;
+}
+ 
+/* See https:// www.geeksforgeeks.org/?p=3622 for details of this
+function */
+PlayerPointer* SortedMerge(PlayerPointer* a, PlayerPointer* b, Player* root){
+    PlayerPointer* result = NULL;
+ 
+    /* Base cases */
+    if (a == NULL)
+        return (b);
+    else if (b == NULL)
+        return (a);
+ 
+    /* Pick either a or b, and recur */
+	Player* ap = findPlayerById(root, a->id_player);
+	Player* bp = findPlayerById(root, b->id_player);
+    if ((float)ap->rating/ap->count <= (float)bp->rating/bp->count) {
+        result = b;
+        result->next = SortedMerge(a, b->next, root);
+    }
+    else {
+        result = a;
+        result->next = SortedMerge(a->next, b, root);
+    }
+    return (result);
 }
